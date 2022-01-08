18171e730e6bf00efe5a74d45febdaddba028120
HEUGUET-Antonin <antonin.heuguet@edu.univ-fcomte.fr>
2022-01-08 19:36:15 +0100

    Page 404 et carrousel fonctionne

---
+++ src/js/fonctions.js
@@ -0,0 +1,27 @@
+function carrousel() {
+    let images = document.querySelectorAll('.image155');
+    let d = 2000;
+    let delta = 1000;
+
+    images.forEach(function(img,ind) {
+        img.style.zIndex = images.length - ind;
+    });
+
+    let anim15 = anime.timeline({
+        targets: '.imgalt',
+        loop: true,
+        direction: 'alternate',
+        easing: 'linear',
+        delay: function(img, ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return 2*delta + d;
+            if (ind == 2) return 3*delta +2*d
+            if (ind == 3) return 4*delta + 3*d;
+            return 5*delta + 4*d;
+        }
+    });
+    anim15.add({
+        translateX: [{value: 500, duration: d, easing: 'easeInOutSine'}, 
+                     {value: 500, duration: 0, delay: delta}]
+    });
+}
