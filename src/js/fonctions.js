7a55135359f699969e4880435843093ed1bb0840
HEUGUET-Antonin <antonin.heuguet@edu.univ-fcomte.fr>
2022-01-09 00:22:37 +0100

    Carrousel autres projets terminé

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -17,7 +17,10 @@ function carrousel() {
             if (ind == 1) return 2*delta + d;
             if (ind == 2) return 3*delta +2*d
             if (ind == 3) return 4*delta + 3*d;
-            return 5*delta + 4*d;
+            if (ind == 4) return 5*delta + 4*d;
+            if (ind == 5) return 6*delta +5*d
+            if (ind == 6) return 7*delta + 6*d;
+            return 7*delta + 6*d;
         }
     });
     anim15.add({
