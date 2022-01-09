58df2e248cf23493e9b2b8e5593d8db5aedf54e4
HEUGUET-Antonin <antonin.heuguet@edu.univ-fcomte.fr>
2022-01-09 22:02:21 +0100

    Correction de bugs et ajout frise

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -21,7 +21,8 @@ function carrousel() {
             if (ind == 5) return 6*delta +5*d
             if (ind == 6) return 7*delta + 6*d;
             if (ind == 7) return 8*delta + 7*d;
-            return 8*delta + 7*d;
+            if (ind == 8) return 9*delta + 8*d;
+            return 9*delta + 8*d;
         }
     });
     anim15.add({
